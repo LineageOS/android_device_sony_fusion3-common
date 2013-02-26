97756108c18e1194fa7c8fe2aba70b92acbf7101
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-02-26 22:36:02 +0100

    fusion3: power: move to common path
    Change-Id: I47ea26bac8a1eced4df60c3a616d6010c9843eea

--- power/Android.mk
+++ power/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
 LOCAL_MODULE := power.qcom
