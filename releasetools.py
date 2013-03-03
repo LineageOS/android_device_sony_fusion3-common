b0626f8eb9d5ad9899040f173ee4dbe35b7a972b
Andreas Makris <andreas.makris@gmail.com>
2013-03-03 13:59:45 +0200

    Typo fix
    Change-Id: Idb29713ad21a1fa32bdebcd70c7a9ddb0de2d36f

--- releasetools.py
+++ releasetools.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Custom OTA Package commands for blue"""
+"""Custom OTA Package commands for fusion3"""
 
 import common
 import os
