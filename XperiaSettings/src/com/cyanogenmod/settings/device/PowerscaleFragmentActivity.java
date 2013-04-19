168f84458433a119459d00e2059fd9bc946af525
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-04-20 00:46:16 +0200

    [WIP]: add XperiaSettings for testing purposes
    Powerscale conservative policy
    
    Change-Id: I4b853f9f70a0a22e7533c27a4b647b1ef9328e5e

---
+++ XperiaSettings/src/com/cyanogenmod/settings/device/PowerscaleFragmentActivity.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+
+import com.cyanogenmod.settings.device.R;
+
+public class PowerscaleFragmentActivity extends PreferenceFragment {
+
+    private static final String PREF_ENABLED = "1";
+    private static final String TAG = "XperiaSettings_Powerscale";
+    private mPowerscalePolicy mmPowerscalePolicy;
+    private mPowerscalePolicyConservative mmPowerscalePolicyConservative;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.powerscale_preferences);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mmPowerscalePolicy = (mPowerscalePolicy) findPreference(DeviceSettings.KEY_POWERSCALE_POLICY);
+        mmPowerscalePolicy.setEnabled(mPowerscalePolicy.isSupported());
+
+        mmPowerscalePolicyConservative = (mPowerscalePolicyConservative) findPreference(DeviceSettings.KEY_CONSERVATIVE_POLLING);
+        mmPowerscalePolicyConservative.setEnabled(mPowerscalePolicyConservative.isSupported());
+    }
+
+    public static boolean isSupported(String FILE) {
+        return Utils.fileExists(FILE);
+    }
+
+    public static void restore(Context context) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+}
