9cbd9158773d3a84fb5af27c266be29bd5ec0481
Chirayu Desai <cdesai@cyanogenmod.org>
2013-05-03 14:51:39 +0530

    remove releasetools extensions
    Depends-on: Ic348d31e8df7933dc4f9f055b7a5a2beacc21d32
    http://review.cyanogenmod.org/36813
    
    Change-Id: I8cb79d1eaa61caa738f39b50d143dba46ff9610f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,7 +86,6 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/
 # Custom boot
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_BOOTIMG_MK := device/sony/fusion3-common/custombootimg.mk
-TARGET_RELEASETOOLS_EXTENSIONS := device/sony/fusion3-common
 BOARD_CUSTOM_GRAPHICS := ../../../device/sony/fusion3-common/recovery/graphics.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
