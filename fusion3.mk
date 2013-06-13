20bf61f9a36506c4520dcb5c32f7b14cebd81c3f
Chirayu Desai <cdesai@cyanogenmod.org>
2013-06-13 11:39:17 +0530

    add init.qcom.usb.sh
    Change-Id: Idb61faa3b3bae72c9d335f976790275f9fcad7e9

--- fusion3.mk
+++ fusion3.mk
@@ -155,7 +155,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
     $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
-    $(COMMON_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
+    $(COMMON_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    $(COMMON_PATH)/rootdir/system/etc/init.qcom.usb.sh:system/etc/init.qcom.usb.sh
 
 # Post recovery script
 PRODUCT_COPY_FILES += \
