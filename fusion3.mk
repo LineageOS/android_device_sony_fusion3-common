73e08ca053d613d03c0eca9058d50f724041b582
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-09-06 11:14:21 +0200

    fusion3: enable Sony system_monitor
    also disable QCOM thermald
    
    Change-Id: I1f3fd18d7e4546c7dfadd5be2004bfa5416b4ecb

--- fusion3.mk
+++ fusion3.mk
@@ -179,7 +179,8 @@ PRODUCT_COPY_FILES += \
 
 # Thermal monitor configuration
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/etc/thermald.conf:system/etc/thermald.conf
+    $(COMMON_PATH)/rootdir/system/etc/thermald.conf:system/etc/thermald.conf \
+    $(COMMON_PATH)/rootdir/system/etc/disable_msm_thermal.sh:system/etc/disable_msm_thermal.sh
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
