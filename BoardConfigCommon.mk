037454f3ea861071efdee31a5e8d54020041ee44
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-10-22 12:27:09 +0200

    fusion3: cleanup recovery stuff
    Change-Id: I30a24af98623f5b7c70aa669fe091644d7a1c975

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,7 +89,6 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/
 # Custom boot
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_BOOTIMG_MK := device/sony/fusion3-common/custombootimg.mk
-BOARD_CUSTOM_GRAPHICS := ../../../device/sony/fusion3-common/recovery/graphics.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
 TARGET_RECOVERY_FSTAB := device/sony/fusion3-common/rootdir/fstab.qcom
