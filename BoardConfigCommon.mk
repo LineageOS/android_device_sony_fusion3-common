6cd58957ce46529e0d3aad0eec319876559937cb
Chirayu Desai <cdesai@cyanogenmod.org>
2013-11-07 19:22:37 +0530

    Set FPU to neon
    Change-Id: I4d7983651c078e9e6bbf00772f2f966e9c803cff

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,8 +34,8 @@ BOARD_LIB_DUMPSTATE := libdumpstate.sony
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 
 # Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 TARGET_CPU_VARIANT := krait
 
