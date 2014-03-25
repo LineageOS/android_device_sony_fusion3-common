849051bd694ce42914ae6cd96131d4f019139bb9
sssemil <suleymanovemil8@gmail.com>
2014-03-25 10:22:28 +0100

    fusion3: IR Remote support
    Change-Id: Ie0a86e1daeccfc20952a98e846c2fdf7bea8d47b

--- fusion3.mk
+++ fusion3.mk
@@ -202,5 +202,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.input.noresample=1
 
+# IR-blaster Support
+ifeq ($(BOARD_HAVE_IR_BLASTER),true)
+PRODUCT_PACKAGES += \
+    SonyIRRemote \
+    libjni_sonyopenir
+endif
+
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/fusion3-common/fusion3-common-vendor.mk)
