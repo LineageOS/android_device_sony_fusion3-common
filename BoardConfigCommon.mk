47465f75f87ecb82aaee1f5242b03443643b683b
Vladislav Koldobskiy <admin@nevergone.ru>
2014-06-23 18:55:23 +0400

    fusion3: remove unused flag
    Change-Id: If502396474aeb793344c40334630ff821988d5e3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,9 +30,6 @@ BOARD_VENDOR_PLATFORM := fusion3
 
 BOARD_LIB_DUMPSTATE := libdumpstate.sony
 
-# Architecture
-TARGET_ARCH_VARIANT_CPU := cortex-a9
-
 # Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
