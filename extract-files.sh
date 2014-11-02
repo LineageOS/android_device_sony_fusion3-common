a7cf4b18fd3dba63410a65b5ec9b7d0943564239
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-11-02 15:13:04 +0100

    script: fix traditional sort
    Change-Id: I7d77d933d834d043f219641219e5a00cfc18448c

--- extract-files.sh
+++ extract-files.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-export DEVICE=${PWD##*/}
+# Use tradition sort
+export LC_ALL=C
+
+FP=$(dirname $(readlink -f $0))
+export VENDOR=$(basename $(dirname $FP))
+export DEVICE=$(basename $FP)
 export BOARDCONFIGVENDOR=false
 export BOARD_VENDOR_PLATFORM=fusion3
 
 ../common/extract-files.sh $@
 
 ../common/setup-makefiles.sh
+
 ./setup-makefiles.sh
