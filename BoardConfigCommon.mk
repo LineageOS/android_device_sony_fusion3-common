5c0eb8edbdecf6278410891cf8536b0ed38841d4
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-11-12 10:10:35 +0100

    fusion3: remove twrp flags
    * These aren't maintained / kept in sync with TWRP.
    
    Change-Id: I2082f84513fb881f5bb37584118369d6d5bd398b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -124,29 +124,6 @@ SOMC_CFG_SENSORS_PROXIMITY_APDS9702 := yes
 SOMC_CFG_SENSORS_ACCEL_BMA250NA_INPUT := yes
 SOMC_CFG_SENSORS_COMPASS_AK8963 := yes
 
-# TWRP flags
-DEVICE_RESOLUTION := 1080x1920
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_SDCARD_ON_DATA := true
-TW_HAS_NO_RECOVERY_PARTITION := true
-TW_FLASH_FROM_STORAGE := true
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_DEFAULT_EXTERNAL_STORAGE := true
-# TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_JB_CRYPTO := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/userdata"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
-TW_CRYPTO_FS_FLAGS := "0x00000406"
-TW_CRYPTO_KEY_LOC := "footer"
-TW_INCLUDE_FUSE_EXFAT := true
-TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/fusion3-common/recovery/twrpgraphics.c
-TW_BRIGHTNESS_PATH := /sys/devices/i2c-0/0-0036/leds/lm3533-lcd-bl/brightness
-TW_MAX_BRIGHTNESS := 255
-TW_NO_USB_STORAGE := true
-
 BOARD_SEPOLICY_DIRS += \
     device/sony/fusion3-common/sepolicy
 
