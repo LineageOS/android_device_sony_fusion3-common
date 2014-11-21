42ea0a9c645a81542cb77803b30e50f8bf0c03cf
iKillCypher <soul52134@gmail.com>
2014-11-21 17:59:59 +0800

    TEMPORARY: set selinux permissive
    Change-Id: I210b4d7326562ec24cadfbc663806b41d79d6574

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,7 +47,7 @@ TARGET_KRAIT_BIONIC_PLDSIZE   := 64
 # Kernel information
 BOARD_KERNEL_BASE     := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE  := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2
+BOARD_KERNEL_CMDLINE  := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000
 
 # Wifi
