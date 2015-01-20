12c4d6d268735e972aa57018adebe76f3787298f
Scott Mertz <scott@cyngn.com>
2015-01-20 13:05:13 -0800

    oppo-common: cmhw: Don't cache tap-to-wake setting
    Change-Id: I0af3b462e31d9e89133bc5278795a3afa5461890

--- cmhw/org/cyanogenmod/hardware/TapToWake.java
+++ cmhw/org/cyanogenmod/hardware/TapToWake.java
@@ -21,18 +21,16 @@ import org.cyanogenmod.hardware.util.FileUtils;
 public class TapToWake {
 
     private static String CONTROL_PATH = "/sys/devices/virtual/input/clearpad/wakeup_gesture";
-    private static boolean mEnabled = false;
 
     public static boolean isSupported() {
         return true;
     }
 
     public static boolean isEnabled()  {
-        return mEnabled;
+        return Integer.parseInt(FileUtils.readOneLine(CONTROL_PATH)) == 1;
     }
 
     public static boolean setEnabled(boolean state)  {
-        mEnabled = state;
         return FileUtils.writeLine(CONTROL_PATH, (state ? "1" : "0"));
     }
 }
