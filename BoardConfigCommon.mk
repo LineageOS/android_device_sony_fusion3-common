b373973ac0c84e253d67f9af94ff3489a857bfa7
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-01-31 10:49:33 +0000

    Revert "temporary set selinux permissive"
    This reverts commit b41142b636bd537ba94bb362e2051e211ea5dfb6.
    
    Change-Id: I6b290871e79e69b241ced6aceb4eec4b583d642d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -33,7 +33,7 @@ TARGET_CPU_VARIANT := krait
 # Kernel information
 BOARD_KERNEL_BASE     := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE  := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE  := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000
 TARGET_KERNEL_SOURCE  := kernel/sony/apq8064
 
