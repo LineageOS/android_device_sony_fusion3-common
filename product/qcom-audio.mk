a088b404e667c52eb5c9b7e33db4f093eb979b17
Mike Lockwood <lockwood@google.com>
2015-03-27 13:18:43 -0700

    fusion3: Enable MIDI support
     * Includes commits :
       - Iebfb15fcc2c8b793c8392cbba7991fc669e7f72f
       - Ibcf74af9e7f4537497c4cf35777e0ffd29858847
    
    Change-Id: I64d02681451f461dd0518576962a81d6bc0320f3

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,6 +1,7 @@
 # Audio permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
 
 # Audio packages
 PRODUCT_PACKAGES += \
