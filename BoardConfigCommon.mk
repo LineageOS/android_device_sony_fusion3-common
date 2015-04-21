eba4179486b832f622a3bd33c4b2ceb35ea1b074
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-04-21 16:12:20 +0200

    import updated libril
    after removing libril from msm8960-common, pull in updated v11
    for fusion3. fixes radio on all fusion3 devices.
    
    commit 6365145486d7b746d3ec5205edb9acfcb06de10c
    from https://github.com/adrian-bl-yuga/android_hardware_ril
    
    Change-Id: I9759958fa1ea11e11770486d0a96f0ebf4766c7f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,7 +53,8 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 HAVE_ADRENO_SOURCE := false
 
 # RIL
-BOARD_HAS_RIL_LEGACY_PAP := true
+BOARD_PROVIDES_LIBRIL := true
+BOARD_RIL_CLASS := ../../../device/sony/fusion3-common/ril/
 
 # Sensors
 SOMC_CFG_SENSORS := true
