aa98a82a51c33a3c6d040c42045082e3567606c6
Steven Veigas <sveiga@codeaurora.org>
2015-07-10 11:04:32 +0000

    fusion3: board: Disable serial logs for faster boot
     * Serial logs at kernel are disabled. Change 'BOARD_ENABLE_SERIAL_CONSOLE'
        to 'true' to enable serial console
    
    Change-Id: Ie9fb7d8f660f5896b12ff2db520e1e84b5939569

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,8 +1,14 @@
 # Kernel informations
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+
+# Serial console
+BOARD_ENABLE_SERIAL_CONSOLE := false
+ifeq ($(BOARD_ENABLE_SERIAL_CONSOLE),true)
+BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8
+endif
 
 # Kernel properties
 TARGET_KERNEL_SOURCE := kernel/sony/apq8064
