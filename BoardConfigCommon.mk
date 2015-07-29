e75cdbdc1bb58e29ec179f40b0411d7f31633850
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-07-29 23:26:43 +0200

    charger: configure led sysfs
    Change-Id: I853e7a982524d8c5e6e0f22df500efcf034b4adf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,6 +77,9 @@ HAVE_ADRENO_SOURCE := false
 
 # Healthd
 BOARD_CHARGER_ENABLE_SUSPEND := true
+RED_LED_PATH := /sys/class/leds/lm3533-red/brightness
+GREEN_LED_PATH := /sys/class/leds/lm3533-green/brightness
+BLUE_LED_PATH := /sys/class/leds/lm3533-blue/brightness
 
 # Lights HAL
 TARGET_PROVIDES_LIBLIGHT := true
