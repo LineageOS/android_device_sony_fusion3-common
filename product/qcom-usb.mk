e1169b766406873569c56ff9812a08a67bb797bf
David Viteri <davidteri91@gmail.com>
2015-10-10 23:16:14 +0200

    fusion3: Remove double packages
     * Those already defined by AOSP
    
     * librs_jni --> https://android.googlesource.com/platform/build/+/android-6.0.0_r1/target/product/generic_no_telephony.mk#39
    
     * com.android.future.usb.accessory --> https://android.googlesource.com/platform/build/+/android-6.0.0_r1/target/product/core_minimal.mk#36
    
    Change-Id: I2a174c4971272583d98c6fc2dcf42ea29f4fe3cf
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- product/qcom-usb.mk
+++ product/qcom-usb.mk
@@ -2,8 +2,3 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
-
-# USB packages
-PRODUCT_PACKAGES += \
-    librs_jni \
-    com.android.future.usb.accessory
