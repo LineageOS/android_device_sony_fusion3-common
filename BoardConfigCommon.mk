c9815abb7e749171679b6834079a45356c2ea049
AdrianDC <radian.dc@gmail.com>
2015-11-24 20:23:39 +0100

    fusion3: Implement the healthd offline charger
     * Use custom resources created out of the original SONY charger
     * Allow Android to properly boot directly from the offline charger
    
     * Thanks to Michael Bestas for the reboot hints
    
     * Implement the libhealthd.msm static library
    
     * Includes :
         I3bba3b58daba6cffa4eb1314a6da1542f43e8e47
         I6c3bf6abbb4e11dc010c360ecfe9f3bb3e2f3548
         Ibf2ee853657c602158af219de0ba132f29dc4571
    
    Change-Id: Icf01407e57ba94274d3ca558fbf71be2864601d0
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    Signed-off-by: Chippa-a <vusal1372@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,9 @@ HAVE_ADRENO_SOURCE := false
 
 # Healthd
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := device/sony/fusion3-common/charger/images
 RED_LED_PATH := /sys/class/leds/lm3533-red/brightness
 GREEN_LED_PATH := /sys/class/leds/lm3533-green/brightness
 BLUE_LED_PATH := /sys/class/leds/lm3533-blue/brightness
