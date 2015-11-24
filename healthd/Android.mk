7c8d862f5536eb3a21914bec02c06ab0f27bee92
AdrianDC <radian.dc@gmail.com>
2015-11-24 20:23:39 +0100

    fusion3: Implement the healthd offline charger
     * Use custom resources created out of the original SONY charger
     * Allow Android to properly boot directly from the offline charger
    
     * Thanks to Michael Bestas for the reboot hints
    
     * Implement the libhealthd.msm static library
    
     * Includes :
         I3bba3b58daba6cffa4eb1314a6da1542f43e8e47
         I6c3bf6abbb4e11dc010c360ecfe9f3bb3e2f3548
         Ibf2ee853657c602158af219de0ba132f29dc4571
    
    Change-Id: Icf01407e57ba94274d3ca558fbf71be2864601d0
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

---
+++ healthd/Android.mk
@@ -0,0 +1,16 @@
+# Copyright 2013 The Android Open Source Project
+# Copyright 2016 The CyanogenMod Project
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := healthd_board_msm.cpp
+LOCAL_MODULE := libhealthd.msm
+LOCAL_C_INCLUDES := system/core/healthd
+LOCAL_CFLAGS := -Werror
+
+ifneq ($(SECONDARY_BACKLIGHT_PATH),)
+    LOCAL_CFLAGS += -DHEALTHD_SECONDARY_BACKLIGHT_PATH
+endif
+
+include $(BUILD_STATIC_LIBRARY)
