1e331831f70379b977521da5ddcc53ec77921455
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-11-25 23:47:33 +0100

    camera: shim libraries for marshmallow
    Thanks for figuring out the Looper and do_mmap_ion issue:
    - Otto Juntunen <otsoni@gmail.com>
    Thanks for figuring out the do_mmap_ion issue:
    - Adrian DC <radian.dc@gmail.com>
    
    
    Change-Id: Iff22294cd5bffbdf54483d22168a59dab1bd47f2

---
+++ libshims/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    graphic-buffer.cpp \
+    utils/Looper.cpp
+
+LOCAL_SHARED_LIBRARIES := libui libutils liblog
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    gui/SensorManager.cpp \
+    parcel.cpp
+
+LOCAL_SHARED_LIBRARIES := libcutils libgui libbinder libutils
+LOCAL_MODULE := libshim_cald
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
