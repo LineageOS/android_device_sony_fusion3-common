e30254ad7f2aefeb770f55db0fdc3c5e9a180b9c
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-11-25 23:47:33 +0100

    camera: getting shimmy with it
    Change-Id: Iff22294cd5bffbdf54483d22168a59dab1bd47f2

---
+++ libshims/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    graphic-buffer.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libui libgui libbinder libutils
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    gui/SensorManager.cpp \
+    parcel.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
+LOCAL_MODULE := libshim_cald
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
