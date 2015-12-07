c01f8674babd9912c597a0a29b77c69d788eb09b
Olivier K <olivier@olivierk.eu>
2015-12-07 19:31:16 +0100

    libril: Update for M
    Sync with https://github.com/CyanogenMod/android_hardware_ril/tree/0289ec9ab72a1135cc8c06222cdf970a78b7b5d7.
    
    This is a squash of the following commits:
    
    6568c79: AdrianDC: libril: fix debug broken lines
    ce84fb4: Pavel Zhamaitsiak: Fix memory leaks and use of uninitialized pointers.
    7e44b78: Nathan Harold: Modify RIL Unsol Message Wakelock to 200ms
    86e1428: Sungmin Choi: Support backward compatibility of new LAST_CALL_FAIL_CAUSE
    106cd45: Chao Liu: Add an optional vendor_cause string to RIL_REQUEST_LAST_CALL_FAIL_CAUSE's response. This vendor cause string will be passed to Telecom's DisconnectCause, which could help us figure out why we are seeing so many ERROR_UNSPECIFIED. bug:17701976
    1951afe: Prerepa Viswanadham: Modem activity info
    d423378: Chih-Hung Hsieh: Add missing extern "C" declaration.
    d7606c8: Robert Greenwalt: Remove verbose logs
    0892816: fenglu: Link Capacity Estimation - RIL support
    be5ce5e: Vinit Deshpande: Fix build issues after the merge
    3e7e8b9: Mark Salyzyn: libril: tag RILC messages to radio log
    14d1d14: Dheeraj Shetty: Introduction of the new SAP-UIM socket
    419b99c: Nick Kralevich: ril.cpp: fix misuse of strncat
    1837b6b: Nanik Tolaram: Removal of dead code and adding log messages to make it easier for debugging
    3584e6b: Elliott Hughes: Remove makefile cruft.
    0645cc6: Elliott Hughes: We have POSIX clocks.
    
    Change-Id: I51aba73ef7a7b6c490231433d0174d4742ca3caa

--- libril/Android.mk
+++ libril/Android.mk
@@ -7,7 +7,9 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
     ril.cpp \
-    ril_event.cpp
+    ril_event.cpp \
+    RilSocket.cpp \
+    RilSapSocket.cpp \
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
@@ -15,7 +17,10 @@ LOCAL_SHARED_LIBRARIES := \
     libbinder \
     libcutils \
     libhardware_legacy \
-    librilutils
+    librilutils \
+
+LOCAL_STATIC_LIBRARIES := \
+    libprotobuf-c-nano-enable_malloc \
 
 #LOCAL_CFLAGS := -DANDROID_MULTI_SIM -DDSDA_RILD1
 
@@ -23,32 +28,17 @@ ifeq ($(SIM_COUNT), 2)
     LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
 endif
 
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADER)/librilutils
+LOCAL_C_INCLUDES += external/nanopb-c
+
 ifeq ($(BOARD_HAS_RIL_LEGACY_PAP),true)
     LOCAL_CFLAGS += -DRIL_LEGACY_PAP
 endif
 
 LOCAL_MODULE:= libril
 
+LOCAL_COPY_HEADERS_TO := libril
+LOCAL_COPY_HEADERS := ril_ex.h
+
 include $(BUILD_SHARED_LIBRARY)
-
-
-# For RdoServD which needs a static library
-# =========================================
-ifneq ($(ANDROID_BIONIC_TRANSITION),)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-    ril.cpp
-
-LOCAL_STATIC_LIBRARIES := \
-    libutils_static \
-    libcutils \
-    librilutils_static
-
-LOCAL_CFLAGS :=
-
-LOCAL_MODULE:= libril_static
-
-include $(BUILD_STATIC_LIBRARY)
-endif # ANDROID_BIONIC_TRANSITION
 endif # BOARD_PROVIDES_LIBRIL
