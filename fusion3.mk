c3c95f508318db2afa465d4db617f45c9bce5ed8
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2016-04-12 08:09:02 +0200

    camera: use the new app Snap
    Change-Id: I248cd8bd1f7da6b0f4e89cd93a1e666bff54a834

--- fusion3.mk
+++ fusion3.mk
@@ -43,6 +43,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.google.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
+# Camera app
+PRODUCT_PACKAGES += \
+    Snap
+
 # Camera wrapper
 PRODUCT_PACKAGES += \
     camera.qcom \
