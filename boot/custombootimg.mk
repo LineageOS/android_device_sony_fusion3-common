670f9d616425247dd6d1ee220dcc7f2116265ba5
RonGokhale <cips173@gmail.com>
2016-04-29 00:50:58 +0530

    boot: Don't create boot.elf link
     - it's leftover
    
    Change-Id: Ief76d497eb2dd5c5ad80700b77f53cf59ca5b196

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -57,8 +57,6 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
 	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/combinedroot.fs --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_BOOTIMAGE_TARGET)
 
-	$(hide) ln -f $(INSTALLED_BOOTIMAGE_TARGET) $(PRODUCT_OUT)/boot.elf
-
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
 	$(call pretty,"Made boot image: $@")
 
