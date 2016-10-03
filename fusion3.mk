1d794b800b2836c67e6f5e3d5afa953d69c4d5ea
Ethan Chen <intervigil@gmail.com>
2016-10-03 14:50:13 -0700

    fusion3: bluetooth: Set Qualcomm Bluetooth SOC ID
    Change-Id: Ic9b0a343ef6e1abed1cfa645028fec5442c31eeb

--- fusion3.mk
+++ fusion3.mk
@@ -269,6 +269,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
+    qcom.bluetooth.soc=smd \
     ro.qualcomm.bt.hci_transport=smd \
     ro.bt.bdaddr_path=/data/misc/bluetooth/bdaddr
 
