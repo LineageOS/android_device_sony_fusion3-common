bbfdc54ce401399d29993a023133ba3798b9552c
Martijn Coenen <maco@google.com>
2016-10-12 01:09:11 +0200

    nfc: Add default implementation.
    Bug: 32095347
    Test: NFC works again.
    Change-Id: I9537ff82424195ebbded7294f38636c72fdb64c4

--- product/qcom-nfc.mk
+++ product/qcom-nfc.mk
@@ -5,6 +5,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
+# NFC HAL
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl
+
 # NFC packages
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
