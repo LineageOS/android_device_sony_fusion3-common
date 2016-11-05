d0d84ac7abb957ed7fccbed4d72e6b19e26c96ae
Markus Menzel <markusmenzel@gmail.com>
2016-11-05 04:48:59 -0700

    fusion3: Use jemalloc instead of dlmalloc
    MALLOC_IMPL is deprecated
    
    Change-Id: I0622c5673cbf589bc783626261a842aed3a67c18

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,9 +35,6 @@ TARGET_BOARD_PLATFORM := msm8960
 BOARD_VENDOR_PLATFORM := fusion3
 BOARD_LIB_DUMPSTATE := libdumpstate.sony
 
-# Bionic
-MALLOC_IMPL := dlmalloc
-
 # Kernel information
 BOARD_KERNEL_BASE     := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
