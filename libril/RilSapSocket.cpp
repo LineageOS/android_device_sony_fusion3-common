f4e8346f056bc3e4870fdf2fca27d919bd12a977
Chippa-a <vusal1372@gmail.com>
2016-11-06 21:45:00 +0300

    fusion3: Update libril sources to android-7.1.0_r4
    This is a squash of the following commits:
      * f5d7ea3: Prepare to remove ril from global include paths
      * 8593f26: libril: fix a syntax error
      * fa49a7f: Fix for rild crash becasue of buffer overflow
      * 606dd67: Fix build error with RILC_LOG on
      * b8ade03: Properly export ril_ex.h
      * 4c05f35: Improve RIL versioning.
      * 638a723: RilSapSocket: Send response with empty payload
      * 0eb4fba: Add WRITE_SMS_TO_SIM and UNSOL_SUPP_SVC_NOTIFICATION
      * 66701c5: Use SimRefreshResponse struct in RIL Versions greater than 7
      * 0090961: Added fields to CELL_INFO_LIST.
      * 6ff9a87: Improve wakelock symantics in RIL.
      * 605c784: Add support for cdma dynamic address length.
      * d8c0b46: Use different response type for unsolicited mesg requiring wakelock.
      * b39e5c9: Convert newly added error code to string values in ril.cpp.
      * 0106aed: Add OEM specific error code space which can by used to replace Generic errors.
      * 55227b5: Fixing security vulnerabilities in ril.cpp
      * e52a6c9: Remove log spam in responseCellInfoList in ril.cpp
      * 0cfc553: Fix security vulnerabilities in ril.cpp
      * 2f0b452: Enable integer sanitization in libril.
      * df7add0: Add RIL Error code list
      * 68900f5: New error codes for SMS/other messages
      * 1034589: Ril Error codes: Voice related messages
      * da91641: ril : print request name for RIL_REQUEST_NV_RESET_CONFIG
      * 405313f: RIL: release unsol wakelock
      * b4e3431: Add RIL command RIL_REQUEST_SET_CARRIER_RESTRICTIONS and RIL_REQUEST_GET_CARRIER_RESTRICTIONS
      * df8398c: ril: make sure ril response on RIL version error
      * 27e99c5: Add support for UNSOL PCO Data.
      * 5d703cb: Fix dispatchCarrierRestrictions string reading from parcel
      * 626099e: Fix build failure which occurs after setting macro RILC_LOG.
      * b460ba8: Fix responseCarrierRestrictions string wrting to parcel
      * ef966a3: Fix compile warning in libril.cpp
      * f0c8ca7: Fix build warnings in hardware/ril.
      * 49db26c: Replace variable-length arrays on stack with malloc.
      * f366a18: SEEK: Add support for getAtr
      * 89e8898: Add p2 option in openLogicalChannel.
      * f013b08: Allow voice radio tech update in Airplane Mode.
      * a368731: rild: Send extended information records as ByteBuffer.
      * 80d60c9: Optimize SIM phonebook feature with new batch APIs
      * 96fd24b: ril: Fix kw issues
      * b3b6435: libril: Fix KW issues
      * 0a3bd6d: RIL: Fix CellInfo for RIL version < 12
      * a994849: Fix RIL_CardStatus_v5 Parceling.
    
    Change-Id: I4230c93c004a1ad2e50e47a00c37e911b02842a7

--- libril/RilSapSocket.cpp
+++ libril/RilSapSocket.cpp
@@ -21,6 +21,7 @@
 #include "RilSapSocket.h"
 #include "pb_decode.h"
 #include "pb_encode.h"
+#undef LOG_TAG
 #define LOG_TAG "RIL_UIM_SOCKET"
 #include <utils/Log.h>
 #include <arpa/inet.h>
@@ -85,7 +86,9 @@ void RilSapSocket::sOnUnsolicitedResponse(int unsolResponse,
        const void *data,
        size_t datalen) {
     RilSapSocket *sap_socket = getSocketById(RIL_SOCKET_1);
-    sap_socket->onUnsolicitedResponse(unsolResponse, (void *)data, datalen);
+    if(sap_socket){
+        sap_socket->onUnsolicitedResponse(unsolResponse, (void *)data, datalen);
+    }
 }
 #endif
 
@@ -248,7 +251,7 @@ void log_hex(const char *who, const uint8_t *buffer, int length) {
     int per_line = 0;
 
     do {
-        dest += sprintf(out, "%8.8s [%8.8x] ", who, source);
+        dest += snprintf(out, sizeof(out), "%8.8s [%8.8x] ", who, source);
         for(; source < length && dest_len - dest > 3 && per_line < BYTES_PER_LINE; source++,
         per_line ++) {
             out[dest++] = HEX_HIGH(buffer[source]);
@@ -300,25 +303,26 @@ void RilSapSocket::onRequestComplete(RIL_Token t, RIL_Errno e, void *response,
     SapSocketRequest* request= (SapSocketRequest*)t;
     MsgHeader *hdr = request->curr;
 
-    if (response && response_len > 0) {
-        MsgHeader rsp;
-        rsp.token = request->curr->token;
-        rsp.type = MsgType_RESPONSE;
-        rsp.id = request->curr->id;
-        rsp.error = (Error)e;
-        rsp.payload = (pb_bytes_array_t *)calloc(1,
-                sizeof(pb_bytes_array_t) + response_len);
-        if (!rsp.payload) {
-            RLOGE("onRequestComplete: OOM");
-        } else {
+    MsgHeader rsp;
+    rsp.token = request->curr->token;
+    rsp.type = MsgType_RESPONSE;
+    rsp.id = request->curr->id;
+    rsp.error = (Error)e;
+    rsp.payload = (pb_bytes_array_t *)calloc(1, sizeof(pb_bytes_array_t) + response_len);
+    if (!rsp.payload) {
+        RLOGE("onRequestComplete: OOM");
+    } else {
+        if (response && response_len > 0) {
             memcpy(rsp.payload->bytes, response, response_len);
             rsp.payload->size = response_len;
-
-            RLOGE("Token:%d, MessageId:%d", hdr->token, hdr->id);
-
-            sendResponse(&rsp);
-            free(rsp.payload);
+        } else {
+            rsp.payload->size = 0;
         }
+
+        RLOGE("Token:%d, MessageId:%d", hdr->token, hdr->id);
+
+        sendResponse(&rsp);
+        free(rsp.payload);
     }
 
     // Deallocate SapSocketRequest
@@ -343,15 +347,20 @@ void RilSapSocket::sendResponse(MsgHeader* hdr) {
     if ((success = pb_get_encoded_size(&encoded_size, MsgHeader_fields,
         hdr)) && encoded_size <= INT32_MAX && commandFd != -1) {
         buffer_size = encoded_size + sizeof(uint32_t);
-        uint8_t buffer[buffer_size];
+        uint8_t* buffer = (uint8_t*)malloc(buffer_size);
+        if (!buffer) {
+            RLOGE("sendResponse: OOM");
+            pthread_mutex_unlock(&write_lock);
+            return;
+        }
         written_size = htonl((uint32_t) encoded_size);
         ostream = pb_ostream_from_buffer(buffer, buffer_size);
         pb_write(&ostream, (uint8_t *)&written_size, sizeof(written_size));
         success = pb_encode(&ostream, MsgHeader_fields, hdr);
 
         if (success) {
-            RLOGD("Size: %d (0x%x) Size as written: 0x%x", encoded_size, encoded_size,
-        written_size);
+            RLOGD("Size: %zu (0x%zx) Size as written: 0x%x", encoded_size,
+                    encoded_size, written_size);
             log_hex("onRequestComplete", &buffer[sizeof(written_size)], encoded_size);
             RLOGI("[%d] < SAP RESPONSE type: %d. id: %d. error: %d",
         hdr->token, hdr->type, hdr->id,hdr->error );
@@ -362,12 +371,13 @@ void RilSapSocket::sendResponse(MsgHeader* hdr) {
                 RLOGD("Write successful");
             }
         } else {
-            RLOGE("Error while encoding response of type %d id %d buffer_size: %d: %s.",
-            hdr->type, hdr->id, buffer_size, PB_GET_ERROR(&ostream));
+            RLOGE("Error while encoding response of type %d id %d buffer_size: %zu: %s.",
+                    hdr->type, hdr->id, buffer_size, PB_GET_ERROR(&ostream));
         }
+        free(buffer);
     } else {
-    RLOGE("Not sending response type %d: encoded_size: %u. commandFd: %d. encoded size result: %d",
-        hdr->type, encoded_size, commandFd, success);
+        RLOGE("Not sending response type %d: encoded_size: %zu. commandFd: %d. encoded size result:\
+                %d", hdr->type, encoded_size, commandFd, success);
     }
 
     pthread_mutex_unlock(&write_lock);
@@ -436,7 +446,11 @@ void RilSapSocket::sendDisconnect() {
    if ((success = pb_get_encoded_size(&encoded_size, RIL_SIM_SAP_DISCONNECT_REQ_fields,
         &disconnectReq)) && encoded_size <= INT32_MAX) {
         buffer_size = encoded_size + sizeof(uint32_t);
-        uint8_t buffer[buffer_size];
+        uint8_t* buffer = (uint8_t*)malloc(buffer_size);
+        if (!buffer) {
+            RLOGE("sendDisconnect: OOM");
+            return;
+        }
         written_size = htonl((uint32_t) encoded_size);
         ostream = pb_ostream_from_buffer(buffer, buffer_size);
         pb_write(&ostream, (uint8_t *)&written_size, sizeof(written_size));
@@ -468,6 +482,7 @@ void RilSapSocket::sendDisconnect() {
         else {
             RLOGE("Encode failed in send disconnect!");
         }
+        free(buffer);
     }
 }
 
