5f16b112ec5253caab26e71eb787ea96bcc355d0
Chippa-a <vusal1372@gmail.com>
2016-11-07 18:15:26 +0300

    fusion3: Enable hacks for legacy camera HALv1
    Change-Id: I13e3900607bb781b0d464a1901f235b7a0f27045

--- fusion3.mk
+++ fusion3.mk
@@ -293,5 +293,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
+
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/fusion3-common/fusion3-common-vendor.mk)
