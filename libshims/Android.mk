bb50287320eb4d5244cd39e6427ed28bfef257b4
Sergey Vlasov <sigprof@gmail.com>
2016-11-26 19:34:30 +0300

    fusion3: Add shim for illumination_service
    The proprietary illumination_service binary has a bug - it invokes
    accept() without initializing *addrlen.  Add a shim to fix obviously
    broken *addrlen values so that the call does not fail with EINVAL.
    
    Change-Id: I6c79532bbbdc5077681cfbb6b5954484e23ba6db
    Signed-off-by: Sergey Vlasov <sigprof@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -24,3 +24,14 @@ LOCAL_MODULE := libshim_cald
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    accept.c
+
+LOCAL_MODULE := libshim_illumination_service
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
