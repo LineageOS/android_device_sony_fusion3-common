48856d503992d4aeb4116d6b0b48d7f4873b0b15
Jaekyun Seok <jaekyun@google.com>
2017-02-17 09:44:39 +0900

    vendor: Move etc files not requiring system dependency
    This CL will move the following files.
      - media_profiles.xml
      - p2p_supplicant_overlay.conf
      - wpa_supplicant_overlay.conf
    
    Test: building succeeded and tested.
    Bug: 35369237
    Change-Id: Iafc10cdb29f27de63b0c713a77d6bffaed220213

--- product/media.mk
+++ product/media.mk
@@ -2,4 +2,8 @@
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(COMMON_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
+
+# Media properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.settings.xml=/vendor/etc/media_profiles.xml
