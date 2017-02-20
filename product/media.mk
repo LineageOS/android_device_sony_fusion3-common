0e225b721d54b03246be58dd3ff645006b15a30c
Jaekyun Seok <jaekyun@google.com>
2017-02-20 17:40:31 +0900

    vendor: Move etc files about media codec list
    Test: building succeeded and tested.
    Bug: 35369237
    
    Change-Id: I131b568d332aead6804648f63f51b919647fe787

--- product/media.mk
+++ product/media.mk
@@ -1,7 +1,7 @@
 # Media configurations
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(COMMON_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(COMMON_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(COMMON_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
 
 # Media properties
