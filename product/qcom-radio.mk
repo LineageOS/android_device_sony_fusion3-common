f542f8978ca45b37d877f95759205dc6814e3f7c
CyberWalkMaN <firefox883@gmail.com>
2017-04-16 11:56:46 +0500

    fusion3: Set mobile data to off by default
    Change-Id: I4381f5b480bdf17481c74d8671c3476c4fbba605

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -6,3 +6,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     com.qc.hardware=true \
     persist.data.qmi.adb_logmask=0
+
+# Data properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.android.mobiledata=false
