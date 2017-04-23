6001360ecb023660c9f7bebf1cbb11e880a59906
Adrian DC <radian.dc@gmail.com>
2017-04-23 02:30:04 +0200

    libril: Import AOSP 8.0.0_r3 sources
     * Imported from hardware/ril, revision android-8.0.0_r3
    
    Change-Id: Iadfbf18196a974db7709b466c2b754ddfb1922e6
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- libril/Android.mk
+++ libril/Android.mk
@@ -5,26 +5,34 @@ ifeq ($(BOARD_PROVIDES_LIBRIL),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_VENDOR_MODULE := true
+
 LOCAL_SRC_FILES:= \
     ril.cpp \
     ril_event.cpp\
-    RilSocket.cpp \
     RilSapSocket.cpp \
+    ril_service.cpp \
+    sap_service.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
-    libbinder \
     libcutils \
     libhardware_legacy \
     librilutils \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.deprecated@1.0 \
+    libhidlbase  \
+    libhidltransport \
+    libhwbinder
 
 LOCAL_STATIC_LIBRARIES := \
     libprotobuf-c-nano-enable_malloc \
 
-#LOCAL_CFLAGS := -DANDROID_MULTI_SIM -DDSDA_RILD1
+LOCAL_CFLAGS += -Wno-unused-parameter
 
 ifeq ($(SIM_COUNT), 2)
+    LOCAL_CFLAGS += -DANDROID_MULTI_SIM -DDSDA_RILD1
     LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
 endif
 
@@ -32,14 +40,10 @@ LOCAL_C_INCLUDES += external/nanopb-c
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include
 
-ifeq ($(BOARD_HAS_RIL_LEGACY_PAP),true)
-    LOCAL_CFLAGS += -DRIL_LEGACY_PAP
-endif
-
 LOCAL_MODULE:= libril
 LOCAL_CLANG := true
 LOCAL_SANITIZE := integer
 
 include $(BUILD_SHARED_LIBRARY)
-
 endif # BOARD_PROVIDES_LIBRIL
+
