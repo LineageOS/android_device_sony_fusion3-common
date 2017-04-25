c02b79d6cda83e2ddb471664056e43edb0652718
Chippa-a <vusal1372@gmail.com>
2017-04-25 15:40:31 +0300

    liblights: Switch to open source led HAL
     * Based on AOSP, CAF and sonyxperiadev HAL with
       adaptations for fusion3 board
     * Add required / delete unused permissions
    
    Change-Id: I491de9fd7caa040f8f5a4db2969043f336632a28

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -1,21 +1,36 @@
+# Copyright (C) 2008 The Android Open Source Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ifeq ($(TARGET_PROVIDES_LIBLIGHT),true)
 
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 ifneq ($(strip $(TARGET_DISPLAY_GAMMA_DISABLED)),true)
     LOCAL_CFLAGS += -DENABLE_GAMMA_CORRECTION
 endif
+ifneq ($(SECONDARY_BACKLIGHT_PATH),)
+    LOCAL_CFLAGS += -DSECONDARY_BACKLIGHT
+endif
 
-LOCAL_SRC_FILES := \
-    lights.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libutils libcutils
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_SRC_FILES := lights.c
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := libcutils liblog
+LOCAL_CLANG  := true
 LOCAL_MODULE := lights.msm8960
-
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
