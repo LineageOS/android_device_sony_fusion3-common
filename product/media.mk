4142f786501f6f04e7e90e51f77dd8c2d98206c5
Chippa-a <vusal1372@gmail.com>
2017-04-27 23:16:40 +0300

    fusion3: Switch to using CAF media configurations
     * Update media codecs (save ffmpeg inclusion) and media profiles
     * Add media codec software performance file
       (contains two combined files: media_codecs_google_performance.xml
       and media_codecs_performance_8960.xml)
    
    Configuration files from LA.AF.1.2.1_rb1.26 branch.
    
    Change-Id: I07152467a5460abae94f7fbb2a2ef7202821fd3c

--- product/media.mk
+++ product/media.mk
@@ -1,4 +1,5 @@
 # Media configurations
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(COMMON_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
