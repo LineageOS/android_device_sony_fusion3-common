f5f2cf9dccf472a2da92f8d5cceba705e4fb2883
Chippa-a <vusal1372@gmail.com>
2017-04-27 22:48:04 +0300

    fusion3: Remove clearpad keylayout
     * Key 116 already defined in Generic.kl
       let's remove useless file.
    
    Change-Id: I46a2facbbda232272db8f0e4d0ab77b62a868106

--- product/input.mk
+++ product/input.mk
@@ -1,7 +1,6 @@
 # Keys layouts
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/apq8064-tabla-snd-card_Button_Jack.kl:system/usr/keylayout/apq8064-tabla-snd-card_Button_Jack.kl \
-    $(COMMON_PATH)/configs/clearpad.kl:system/usr/keylayout/clearpad.kl \
     $(COMMON_PATH)/configs/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(COMMON_PATH)/configs/keypad_8960.kl:system/usr/keylayout/keypad_8960.kl \
     $(COMMON_PATH)/configs/mhl-rcp.kl:system/usr/keylayout/mhl-rcp.kl \
