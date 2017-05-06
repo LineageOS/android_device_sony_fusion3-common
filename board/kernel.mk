c912ae3dd1e57096ca2d423291a3f4ccfabf46b1
Kalpaj Chaudhari <daedroza@gmail.com>
2017-05-06 14:34:47 +0530

    fusion3: kernel: Compile with GCC 4.8
    Using GCC 4.9 for linking is not correct. There is documentation from
    Google which recommends using GCC 4.8 for building the kernel. You can
    read about it here:
    https://android.googlesource.com/toolchain/build/+/HEAD/README
    
    The Android toolchain supports the following targets:
       a. arm-linux-androideabi
       b. arm-eabi  (for Android kernel)
       c. arm-newlib-eabi    (for runnng gcc regression tests)
    
    In my previous experiences, the kernel had linking and unwinding
    problems most noticable in AOSP project. Better not use it, or unless
    someone who has expertise can help me fix it.
    Change-Id: I255188af45ea3d742968220e5fa82d2d247b245c

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,6 +3,8 @@ BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
 # Kernel properties
 TARGET_KERNEL_SOURCE := kernel/sony/apq8064
