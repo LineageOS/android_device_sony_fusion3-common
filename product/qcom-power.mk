93a4df7fefaa359c91978eb57ba24d7b4dadb31c
Chippa-a <vusal1372@gmail.com>
2017-09-24 12:44:49 +0300

    fusion3: Remove shim library for mpdecision
     * Not required for new binary.
    
    Change-Id: I400a45a977c1f29fd5c27cabdd21a926f648f27e

--- product/qcom-power.mk
+++ product/qcom-power.mk
@@ -2,10 +2,6 @@
 PRODUCT_PACKAGES += \
     power.qcom
 
-# Mpdecision SHIM packages
-PRODUCT_PACKAGES += \
-    libshim_mpdecision
-
 # QCOM-perf properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
