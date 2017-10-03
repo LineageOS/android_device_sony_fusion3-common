ce9b5209244ca92f3e0aedf50b2d61844d124ac5
Chippa-a <vusal1372@gmail.com>
2017-10-03 18:07:41 +0300

    fusion3: Move tad_static to vendor partition
    Also fix binary permission.
    
    Change-Id: I02058e0cc7030928224613f4228e06ce8401eb66

--- product/platform.mk
+++ product/platform.mk
@@ -5,7 +5,7 @@ PRODUCT_PACKAGES += \
 
 # Sony TrimArea package
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/bin/tad_static:system/bin/tad_static
+    $(COMMON_PATH)/rootdir/vendor/bin/tad_static:$(TARGET_COPY_OUT_VENDOR)/bin/tad_static
 
 # Sony MACAddrSetup package
 PRODUCT_PACKAGES += \
