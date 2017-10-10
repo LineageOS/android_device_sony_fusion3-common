00aba215734d8b03baac36a62b24f4566c443fee
Chippa-a <vusal1372@gmail.com>
2017-10-10 22:03:55 +0300

    vendor: Build sh and mkshrc utilities
     * Required for launching shell scripts from /vendor.
    
    Change-Id: I46ae6fe837cb89a68ae95905d279aaea8a2e707e

--- product/vendor.mk
+++ product/vendor.mk
@@ -1,3 +1,8 @@
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2015-07-10
+
+# Vendor utilities
+PRODUCT_PACKAGES += \
+    mkshrc_vendor \
+    sh_vendor
