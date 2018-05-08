LineageOS/android_device_sony_huashan

vendor: Move etc files about media codec list
Test: building succeeded and tested.
Bug: 35369237

Change-Id: I131b568d332aead6804648f63f51b919647fe787

product/media.mk
@@ -1,13 +1,13 @@
 # Media configurations
 ifneq ($(BOARD_AOSP_BASED),)
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs_performance_aosp.xml:system/etc/media_codecs_performance.xml
+    $(LOCAL_PATH)/configs/media_codecs_performance_aosp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 else
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 endif
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
 
 # Media properties

LineageOS/android_device_sony_fusion3-common

product/media.mk
@@ -1,7 +1,7 @@
 # Media configurations
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(COMMON_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(COMMON_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(COMMON_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
 
 # Media properties