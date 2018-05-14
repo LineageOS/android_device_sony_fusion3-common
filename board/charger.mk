2833b546da031ad77d1d4d3f15908bd360dcec15
Chippa-a <vusal1372@gmail.com>
2018-05-14 23:24:44 +0300

    charger: Use custom healthd charger_res_images
     * Graphics are taken from stock Sony LP
    
    Change-Id: Ib08f16bfe350a4149a66373f7d9b53efd1e438fa

--- board/charger.mk
+++ board/charger.mk
@@ -1,6 +1,7 @@
 # Healthd
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(COMMON_PATH)/charger/images
 RED_LED_PATH := /sys/class/leds/lm3533-red/brightness
 GREEN_LED_PATH := /sys/class/leds/lm3533-green/brightness
 BLUE_LED_PATH := /sys/class/leds/lm3533-blue/brightness
