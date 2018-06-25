e05d4574ed19077bda894b374d03e683207c77b9
Mathieu Chartier <mathieuc@google.com>
2018-06-26 00:14:38 +0200

    board: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914

--- board/build.mk
+++ board/build.mk
@@ -8,5 +8,9 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
+# Always preopt extracted APKs to prevent extracting out of the APK
+# for gms modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
