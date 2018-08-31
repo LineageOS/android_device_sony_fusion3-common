fe74705f4efe6215298a7c99dbb79acb981c7b1c
Danny Baumann <dannybaumann@web.de>
2018-08-31 13:35:20 +0200

    fusion3: Mark camera-related processes as API level 22.
    Our camera blobs trigger some checks for broken behavior (destroyed
    mutex usage, invalid pthread usage) present in newer API levels.
    
    Change-Id: I8705437e0a5c6d80b86748bff559ce1e8a7bf716

--- board/platform.mk
+++ board/platform.mk
@@ -7,6 +7,8 @@ BOARD_VENDOR_PLATFORM := fusion3
 
 # Legacy blobs
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+    /system/bin/mediaserver=22
 
 # Dumpstate
 BOARD_LIB_DUMPSTATE := libdumpstate.sony
