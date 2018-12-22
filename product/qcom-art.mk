04c44841d1c726945be74e73c68a79a8f4da8f2c
Bruno Martins <bgcngm@gmail.com>
2018-12-22 19:07:24 +0000

    fusion3: Enable B-services aging propagation
    Change-Id: I4ac0525933bb421f7d025c5e4c6a1de0f083c46e

--- product/qcom-art.mk
+++ product/qcom-art.mk
@@ -1,4 +1,5 @@
 # ART properties
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false \
-    ro.vendor.qti.am.reschedule_service=true
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.vendor.qti.sys.fw.bservice_enable=true
