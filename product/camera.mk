LineageOS/android_device_sony_huashan

camera: Specify internal video devices to ignore by the external provider
The external camera provider occupies our camera V4L2 nodes, potentially
crashing the kernel driver and blocking the camera HAL.
Unfortunately, there's no easy way to just disable it, so let's just tell
it to ignore the internal video devices.

Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>

product/camera.mk
@@ -8,6 +8,10 @@ PRODUCT_PACKAGES += \
     libshim_cald \
     libshim_camera
 
+# Camera configurations
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Camera properties
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \

LineageOS/android_device_sony_fusion3-common

product/camera.mk
@@ -8,6 +8,10 @@ PRODUCT_PACKAGES += \
     libshim_cald \
     libshim_camera
 
+# Camera configurations
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Camera properties
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.legacyencoder=true \