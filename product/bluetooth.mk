1d9dd18cc749ffe1cfaa1fd56391aaa588e007a8
Arne Coucheron <arco68@gmail.com>
2019-09-09 05:47:48 +0200

    bluetooth: Remove no longer existing BT props
    Change-Id: I56bd1b054910401ff33c855866c755c9ebcfbea8

--- product/bluetooth.mk
+++ product/bluetooth.mk
@@ -4,6 +4,5 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bluetooth.hfp.ver=1.6 \
     ro.bt.bdaddr_path=/data/misc/bluetooth/bdaddr \
     ro.qualcomm.bt.hci_transport=smd
