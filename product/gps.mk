436bd75055bb629317a58383b02dd626f790fa17
cyberwalkman <firefox883@gmail.com>
2019-10-19 23:21:33 +0500

    product: gps: Add missing props
    Change-Id: I1de96b8f817eb0681fc0403770da292fc3cd5b8d

--- product/gps.mk
+++ product/gps.mk
@@ -1,3 +1,8 @@
 # GPS configurations
 PRODUCT_COPY_FILES += \
    $(COMMON_PATH)/gps/gps.conf:system/etc/gps.conf
+
+# GPS properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qc_nlp_in_use=0 \
+    ro.gps.agps_provider=1
