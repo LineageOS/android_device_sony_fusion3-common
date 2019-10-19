0cb8280774cd63a57c751c71aa6e21e92dfcd620
cyberwalkman <firefox883@gmail.com>
2019-10-19 23:50:48 +0500

    product: Add missing props for Wifi Display
    Change-Id: I6c6a7573152bb55e36b52e1000be8446c3c0563e

--- product/display.mk
+++ product/display.mk
@@ -1,4 +1,6 @@
 # Display properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1 \
     persist.hwc.mdpcomp.enable=true \
+    persist.sys.wfd.virtual=0 \
     debug.hwui.use_buffer_age=false
